68730dceff76e086c92cfdafe12b2f0594051159
Bruna Vieira <brunavieiraguedes>
2026-05-02 12:36:10 -0300

    Fix aspas na variavel de ambiente

--- backend/database.py
+++ backend/database.py
@@ -16,8 +16,8 @@ DATABASE = 'VenhaJunto'
 LOCAL_DB_URL = "mssql+pyodbc:///?odbc_connect=Driver%3D%7BODBC+Driver+17+for+SQL+Server%7D%3BServer%3Dlocalhost%3BDatabase%3DVenhaJunto%3BTrusted_Connection%3Dyes%3B"
 
 # Tenta ler a variável de ambiente DATABASE_URL (necessário para a AWS).
-# Se não encontrar, usa o banco local.
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", LOCAL_DB_URL)
+# A função .strip('\"') remove possíveis aspas duplas que o echo colocou no arquivo .env
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", LOCAL_DB_URL).strip('"').strip("'")
 
 # Se for SQLite (na nuvem), precisamos de parâmetros diferentes
 if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
