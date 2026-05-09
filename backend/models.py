255e85c1f751f3996443d2b32c86fcb486e583f2
Bruna Vieira <brunavieiraguedes>
2026-05-09 14:06:17 -0300

    fix: ajustar imports do backend para permitir execução da raiz

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Text, DateTime, Float, Date, UniqueConstraint
 from datetime import datetime, date
 from sqlalchemy.orm import relationship
-from database import Base
+from backend.database import Base
 
 class Usuario(Base):
     __tablename__ = "usuarios"
